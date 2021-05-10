53e0027a8bba89a1a74d79a38c53337ad2681839
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-05-10 20:09:59 +0530

    Two diff input fields with one state, converting state variable to object this time, was very intutive, learnt a lot

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import { useState } from "react";
 
 function App() {
-  const [name, getName] = useState("");
-  const [name2, getName2] = useState("");
-  const [fullName, getFullName] = useState("");
-  const data = (event) => {
-    getName(event.target.value);
-  };
+  const [d, setD] = useState({
+    firstName: "",
+    lastName: "",
+  });
+  const [name, setName] = useState("");
 
-  const data2 = (event) => {
-    getName2(event.target.value);
+  const data = (event) => {
+    let input_name = event.target.name;
+    let input_value = event.target.value;
+
+    setD(function (prev) {
+      if (input_name === "firstName") {
+        return {
+          firstName: input_value,
+          lastName: prev.lastName,
+        };
+      } else {
+        return {
+          firstName: prev.firstName,
+          lastName: input_value,
+        };
+      }
+    });
   };
 
   const takeIt = (event) => {
     event.preventDefault();
-    getFullName(name + " " + name2);
+    setName(d.firstName + " " + d.lastName);
   };
   return (
     <div className="content-center w-screen h-screen grid justify-items-center">
       <div className="text-4xl">
-        Hii, <span className="font-light text-8xl"> {"  " + fullName} </span>
+        Hii, <span className="font-light text-8xl"> {name} </span>
       </div>
       <form onSubmit={takeIt} className="flex flex-col">
         <input
           type="text"
           onChange={data}
           placeholder="kaha likh ra hai, idhar likh .... "
+          name="firstName"
           className="p-4 my-4 text-2xl border-4 rounded-full focus:outline-none focus:border-red-500"
         />
         <input
           type="text"
-          onChange={data2}
+          onChange={data}
+          name="lastName"
           placeholder="password bi likh fata fat"
           className="p-4 my-4 text-2xl border-4 rounded-full focus:outline-none focus:border-red-500"
         />
